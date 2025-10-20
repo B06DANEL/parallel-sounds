a6c46129b7d574da2f9d70ef22f20b3bb05292a6
B06DAN3L2 <ionitabogdanmihai2013@gmail.com>
2025-10-20 20:42:23 +0300

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -4,6 +4,6 @@ for (let index = 0; index <= 7; index++) {
         2 ** ((96 + index) / 12),
         2 ** ((96 + (4 + index)) / 12),
         2 ** ((96 + (7 + index)) / 12),
-        21 ** ((96 + (12 + index)) / 12)
+        2 ** ((96 + (12 + index)) / 12)
     ], 1000, psounds.envelope.piano)
 }
