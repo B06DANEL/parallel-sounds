7dcb950ae1b4c31b88986d48f2efc841ef658c4b
B06DAN3L2 <ionitabogdanmihai2013@gmail.com>
2025-10-20 20:38:11 +0300

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+music.setVolume(127)
+for (let index = 0; index <= 7; index++) {
+    psounds.play_multitone([
+        2 ** ((96 + index) / 12),
+        2 ** ((96 + (4 + index)) / 12),
+        2 ** ((96 + (7 + index)) / 12),
+        21 ** ((96 + (12 + index)) / 12)
+    ], 1000, psounds.envelope.piano)
+}
